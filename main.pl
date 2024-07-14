b88c9e77dc890103ca1fb3e577f88ad803325915
maria.filtsova <maria.filtsova@netrex.cz>
2024-07-14 20:05:51 +0200

    feat: implement termerized html, add css styles

--- main.pl
+++ main.pl
@@ -1,50 +1,68 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_parameters)).
-:- use_module(library(http/http_json)).
-:- use_module(library(http/http_error)).
+:- use_module(library(http/http_files)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/html_head)).
 
 :- dynamic(task/2).
 
-% Define HTTP handlers
-:- http_handler(root(.), home_page, []).
-:- http_handler(root(tasks), tasks_handler, [method(post)]).
+% Routes
+:- http_handler(root(.), main_page, []).
+:- http_handler(root(tasks), add_task_handler, [method(post)]).
 
-% Home page handler
-home_page(_Request) :-
-    format('Content-type: text/html~n~n'),
-    format('<html>
-                <head><title>ToDo List</title></head>
-                <body>
-                    <h2>ToDo List</h2>
-                    <form action="/tasks" method="post">
-                        <label for="task">Task:</label>
-                        <input type="text" id="task" name="task" required>
-                        <input type="submit" value="Add Task">
-                    </form>
-                    <h3>Tasks</h3>
-                    <ul>~n'),
+% Static files
+:- http_handler(static(.), http_reply_from_files(static, []), [prefix]).
+
+app_name('Prolog TODO 📝').
+
+main_page(_Request) :-
+    app_name(Title),
     findall(Task, task(Task, _), Tasks),
-    print_tasks(Tasks),
-    format('</ul>
-            </body>
-            </html>').
+    reply_html_page(
+        % HTML head
+        [title(Title)],
+        % HTML body
+        [
+            \html_requires('/static/style.css'),
+            \page_content(Title, Tasks)
+        ]
+    ).
+    
+page_content(Title, Tasks) -->
+    html(
+        [
+            h2(Title),
+            form([ action='/tasks', method='post' ],
+                [ label([ for='task' ], 'Task: '),
+                    input([ type='text', id='task-input', name='task', required ]),
+                    input([ type='submit', value='Add' ])
+                ]),
+            h3('Tasks'),
+            div([ id='task-list' ],
+                [ ul(\print_tasks(Tasks)) ]
+            )
+        ]
+    ).
 
-% Predicate to print tasks in HTML
-print_tasks([]).
-print_tasks([Task|Rest]) :-
-    format('<li>~w</li>~n', [Task]),
+% DCG to print tasks in HTML
+print_tasks([]) --> [].
+print_tasks([Task|Rest]) -->
+    html(li(Task)),
     print_tasks(Rest).
 
 % Handler for creating tasks
-tasks_handler(Request) :-
+add_task_handler(Request) :-
     http_parameters(Request, [task(Task, [atom])]),
     assertz(task(Task, _)),
-    reply_json_dict(_{status:success}).
+    main_page(Request),
+    http_redirect(see_other, root(.), _).
 
-% Start server
+% Start server on port 8000
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
+:- initialization(server(8000)).
 
-% Example usage: Start server on port 8080
-:- initialization(server(8080)).
+% TODO: add task IDs
+% TODO: edit and delete tasks
+% TODO: csv storage
