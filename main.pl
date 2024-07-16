3fee72820a5c9167f2485166f7b5e68fa0f4f0a1
maria.filtsova <maria.filtsova@netrex.cz>
2024-07-16 16:02:06 +0200

    feat: add task IDs

--- main.pl
+++ main.pl
@@ -1,11 +1,11 @@
-:- use_module(library(http/thread_httpd)).
-:- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_parameters)).
-:- use_module(library(http/http_files)).
-:- use_module(library(http/html_write)).
-:- use_module(library(http/html_head)).
+:- use_module(library(http/thread_httpd)). % for http_server/2
+:- use_module(library(http/http_dispatch)). % for http_handler/3
+:- use_module(library(http/http_parameters)). % for http_parameters/2
+:- use_module(library(http/http_files)). % for static files
+:- use_module(library(http/html_write)). % for html//1
+:- use_module(library(http/html_head)). % for html_requires/1
 
-:- dynamic(task/2).
+:- dynamic(task/2). % task(ID, Description)
 
 % Routes
 :- http_handler(root(.), main_page, []).
@@ -19,7 +19,7 @@ app_name('Prolog TODO 📝').
 
 main_page(_Request) :-
     app_name(Title),
-    findall(Task, task(Task, _), Tasks),
+    findall(Task, task(_, Task), Tasks),
     reply_html_page(
         % HTML head
         [title(Title)],
@@ -54,10 +54,21 @@ print_tasks([Task|Rest]) -->
 
 % Handler for creating tasks
 add_task_handler(Request) :-
-    http_parameters(Request, [task(Task, [atom])]),
-    assertz(task(Task, _)),
+    http_parameters(Request, [task(Description, [atom])]),
+    generate_task_id(NewId), 
+    assertz(task(NewId, Description)),
     main_page(Request),
     http_redirect(see_other, root(.), _).
+
+generate_task_id(NewId) :-
+    (findall(Id, task(Id, _), Ids),
+        % Increment the ID
+        Ids \= [] ->
+            max_list(Ids, MaxId),
+            NewId is MaxId + 1
+        ; % If no tasks exist, start with 1
+            NewId = 1
+    ).
 
 % Start server on port 8000
 server(Port) :-
